aac60b5846d548e1e7678f3858ef1d6783515989
zhaojunping <zhaojunping0622@sina.com>
2020-03-13 17:50:58 +0800

    Callable Demo update

--- src/com/zjp/JUC/CallableDemo.java
+++ src/com/zjp/JUC/CallableDemo.java
@@ -1,20 +1,25 @@
 package com.zjp.JUC;
 
 import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.FutureTask;
 
 class MyThread implements Callable<String>{
 
 	@Override
 	public String call() throws Exception {
-		
-		return null;
+		System.out.println("call method.....");
+		return "aaa";
 	}
 
 }
 public class CallableDemo {
-	//Thread t = new Thread();
-	FutureTask futureTask = new FutureTask(new MyThread());
-	
+	public static void main(String[] args) throws InterruptedException, ExecutionException {
+		//Thread t = new Thread();
+		FutureTask futureTask = new FutureTask(new MyThread());
+		new Thread(futureTask,"A").start();
+		String ret = (String)futureTask.get();
+		System.out.println(ret);
+	}
 	
 }
