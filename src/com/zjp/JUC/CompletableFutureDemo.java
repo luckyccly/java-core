229b60f47934fa1fea4db737dd5613696c3ab52b
zhaojunping <zhaojunping0622@sina.com>
2020-03-31 17:39:48 +0800

    update JUC CompletableFutureDemo and ForkPoolDemo

--- src/com/zjp/JUC/CompletableFutureDemo.java
+++ src/com/zjp/JUC/CompletableFutureDemo.java
@@ -1,7 +1,26 @@
 package com.zjp.JUC;
 
+import java.util.concurrent.CompletableFuture;
+
 public class CompletableFutureDemo {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
+		/* CompletableFuture<Void> completableFuture = CompletableFuture.runAsync(()->{
+			 System.out.println(Thread.currentThread().getName()+"没有返回，update mysql ok");
+		 });
+		 completableFuture.get();*/
 		 
+		 CompletableFuture<Integer> intCompletableFuture = CompletableFuture.supplyAsync(()->{
+			System.out.println(Thread.currentThread().getName()+"有返回， insert mysql ok");
+			//int ret = 24/0;
+			return 1024;
+		 });
+		 
+		 intCompletableFuture.whenComplete((t,u) -> {
+			System.out.println("***********t "+t);
+			System.out.println("***********u "+u);
+		 }).exceptionally(f -> {
+			 System.out.println("*****exception"+f.getMessage());
+			 return 444;
+		 });
 	}
 }
--- src/com/zjp/JUC/ForkPoolDemo.java
+++ src/com/zjp/JUC/ForkPoolDemo.java
@@ -1,9 +1,48 @@
 package com.zjp.JUC;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.ForkJoinTask;
+import java.util.concurrent.RecursiveTask;
+
 /**
  * 分支合并框架
  * */
+class MyTask extends RecursiveTask<Integer>{
+	public static final Integer ADJUST_VALUE = 10;
+	private int begin;
+	private int end;
+	private int result;
+	
+	public MyTask(int begin, int end){
+		this.begin = begin;
+		this.end = end;
+	}
+	@Override
+	protected Integer compute() {
+		if(end - begin < ADJUST_VALUE){
+			for(int i = begin; i <= end; i++){
+				result = result + i;
+			}
+		} else {
+			int middle = (begin + end)/2;
+			MyTask myTask01 = new MyTask(begin, middle);
+			MyTask myTask02 = new MyTask(middle+1, end);
+			myTask01.fork();
+			myTask02.fork();
+			result = myTask01.join() + myTask02.join();
+		}
+		return result;
+	}
+	
+}
 public class ForkPoolDemo {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception{
+		 MyTask task = new MyTask(0, 10);
 		 
+		 ForkJoinPool threadPool = new ForkJoinPool();
+		 ForkJoinTask<Integer> forkJoinTask = threadPool.submit(task);
+		 System.out.println(forkJoinTask.get());
+		 threadPool.shutdown();
 	}
 }
